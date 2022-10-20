b5742ec5884b57753d1af1991829a7fa36e356ba
naphattar <naphatchalermwai@gmail.com>
2022-10-20 23:55:27 +0700

    create login page

---
+++ cugungao/src/Page/Loginandsignuppage/login.js
@@ -0,0 +1,58 @@
+import React from "react";
+import * as Components from './LoginComponents';
+
+ function Login() {
+     const [signIn, toggle] = React.useState(true);
+      return(
+          <Components.Container>
+              <Components.SignUpContainer signinIn={signIn}>
+                  <Components.Form>
+                      <Components.Title>Create Your Account</Components.Title>
+                      <Components.Input type='text' placeholder='Name' />
+                      <Components.Input type='email' placeholder='Email' />
+                      <Components.Input type='password' placeholder='Password' />
+                      <Components.Button>Sign Up</Components.Button>
+                  </Components.Form>
+              </Components.SignUpContainer>
+
+              <Components.SignInContainer signinIn={signIn}>
+                   <Components.Form>
+                       <Components.Title>Sign in</Components.Title>
+                       <Components.Input type='email' placeholder='Email' />
+                       <Components.Input type='password' placeholder='Password' />
+                       <Components.Anchor href='#'>Forgot your password ?</Components.Anchor>
+                       <Components.Button>Sign In</Components.Button>
+                   </Components.Form>
+              </Components.SignInContainer>
+
+              <Components.OverlayContainer signinIn={signIn}>
+                  <Components.Overlay signinIn={signIn}>
+
+                  <Components.LeftOverlayPanel signinIn={signIn}>
+                      <Components.Title>Welcome Back !</Components.Title>
+                      <Components.Paragraph>
+                          Login now and start chating with your new friends
+                      </Components.Paragraph>
+                      <Components.GhostButton onClick={() => toggle(true)}>
+                          Sign In
+                      </Components.GhostButton>
+                      </Components.LeftOverlayPanel>
+
+                      <Components.RightOverlayPanel signinIn={signIn}>
+                        <Components.Title>Welcome !!</Components.Title>
+                        <Components.Paragraph>
+                            Join Us , Just  Sign Up Now
+                        </Components.Paragraph>
+                            <Components.GhostButton onClick={() => toggle(false)}>
+                                Sign Up
+                            </Components.GhostButton> 
+                      </Components.RightOverlayPanel>
+  
+                  </Components.Overlay>
+              </Components.OverlayContainer>
+
+          </Components.Container>
+      )
+ }
+
+ export default Login;
