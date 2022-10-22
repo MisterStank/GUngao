3b742e0b04a76b0f052745b3a36701ebeb510d04
Kriszaaa <tatakrissanapat@gmail.com>
2022-10-22 21:45:02 +0700

    add Pageselect

--- cugungao/src/Page/Home/component/Homepage/Homepage.js
+++ cugungao/src/Page/Home/component/Homepage/Homepage.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Router } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import {Homepagebody, Homepagetext1,Homepagetext2,Homepageparagraph1,Homepageparagraph2} from './HomepageComponent';
+import { Topic,Contain,Text,Selectt, Select_Contain , BtnLink} from './PageselectComponent'
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
 function Homepage(){
     return(
         <div>
@@ -16,6 +23,23 @@ function Homepage(){
                     </Homepageparagraph2>
                 </Homepagetext2>
             </Homepagebody>
+            <Topic>
+                <Select_Contain>
+                    <Contain inside = 'choose'>
+                        <Text>Choose your topic</Text>
+                    </Contain>
+                    <Contain inside = 'select'>
+                        <Selectt
+                            options={options}
+                            placeholder={'Select Topic'}
+                            clearable={true}
+                        />  
+                    </Contain>
+                    <Contain>
+                        <BtnLink to='/chat'>Find my Chat-mate!</BtnLink>
+                    </Contain>
+                </Select_Contain>
+            </Topic>
         </div>
     );
 }
---
+++ cugungao/src/Page/Home/component/Homepage/PageselectComponent.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+import Select from 'react-select'
+import {Link} from 'react-router-dom'
+
+export const Topic = styled.div`
+    background: #D4B588;
+    height: 600px;
+`
+export const Contain = styled.div`
+
+    text-align: center;
+    width: 100%;
+    padding: ${(props) => (props.inside === 'choose' ? '3% 3% 0 3%': props.inside === 'select'? '3% 3% 1% 3%':'1% 3% 3% 3%')};
+    height:${(props) => (props.inside === 'choose' ? 'calc(100%-500px)':null)};
+`
+export const Select_Contain = styled.div`
+    height :inherit;
+    padding : 5%;
+`
+export const Selectt = styled(Select)`
+    margin: 0 auto;
+    width: 500px;
+`
+
+export const Btn = styled.div`
+    display:flex;
+    align-items:center;
+    margin: 0 auto;
+
+`
+export const Text = styled.p`
+    font-weight: bold;
+    font-size: 48px;
+    margin-bottom: 0;
+`
+export const BtnLink = styled(Link)`
+    border-radius : 4px;
+    background : #A96B1E;
+    color: #fff;
+    padding: 10px 22px;
+    border: none;
+    outline: none;
+    text-decoration: none;
+    margin-left: auto;
+    margin-right: auto;
+
+    &:hover{
+        background: #fff;
+        color : #A96B1E;
+    }
+`
